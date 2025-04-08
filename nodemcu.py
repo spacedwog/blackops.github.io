6f47a7c928c288f6afb5aff0b138ccf66c677d81
Felipe Santos <owner@spacedwog.com.br>
2025-04-08 15:49:55 -0300

    Commited

--- nodemcu.py
+++ nodemcu.py
@@ -1,9 +1,10 @@
+# nodemcu.py
 import requests
 
-def enviar_para_esp8266(endpoint: str, payload: dict):
-    url = f"http://192.168.15.8/{endpoint}"  # IP do NodeMCU
+def enviar_para_esp8266(endpoint, payload):
     try:
-        r = requests.post(url, json=payload, timeout=2)
+        url = f"http://192.168.15.8/{endpoint}"
+        r = requests.post(url, json=payload, timeout=5)
         return r.text
-    except requests.RequestException as e:
-        return f"Erro: {e}"
+    except Exception as e:
+        return str(e)
