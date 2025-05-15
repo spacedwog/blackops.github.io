3e424c0dda71be43d2db69657c01d9e997542e8b
Spacedwog <owner@spacedwog.com.br>
2025-05-15 10:03:43 -0300

    Commited

--- cyberops/CyberpunkServer.java
+++ cyberops/CyberpunkServer.java
@@ -13,22 +13,21 @@ public class CyberpunkServer {
             System.out.println("Cyberpunk Java Server is running on port " + port + "...");
 
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                BufferedReader in = new BufferedReader(
-                    new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-                String inputLine = in.readLine();
-                System.out.println("Received: " + inputLine);
-
-                // Lógica de resposta (você pode integrar com métodos Java reais aqui)
-                String response = "[JAVA] Resposta: " + inputLine;
-                out.println(response);
-
-                clientSocket.close();
+                try (Socket clientSocket = serverSocket.accept()) {
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(clientSocket.getInputStream()));
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    
+                    String inputLine = in.readLine();
+                    System.out.println("Received: " + inputLine);
+                    
+                    // Lógica de resposta (você pode integrar com métodos Java reais aqui)
+                    String response = "[JAVA] Resposta: " + inputLine;
+                    out.println(response);
+                }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Ocorreu um erro" + e.toString());
         }
     }
 }
