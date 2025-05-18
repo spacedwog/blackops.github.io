2e92b64c54b35eeaae1b8ee471c524c3c5d5e2bd
Spacedwog <owner@spacedwog.com.br>
2025-05-18 14:07:34 -0300

    Commited

--- cloudops/ThreeBackground.js
+++ cloudops/ThreeBackground.js
@@ -1,47 +1,58 @@
-// ThreeBackground.js
-import React, { useRef, useEffect } from 'react';
-import { View } from 'react-native';
-import { GLView } from 'expo-gl';
-import { Renderer } from 'expo-three';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, Stars, Float, Html } from '@react-three/drei';
 import * as THREE from 'three';
 
-export default function ThreeBackground() {
-  const onContextCreate = async (gl) => {
-    const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
+function SpinningTorus() {
+  const meshRef = useRef();
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 1000);
-    camera.position.z = 2;
-
-    const renderer = new Renderer({ gl });
-    renderer.setSize(width, height);
-
-    // Luz e cubo 3D
-    const ambientLight = new THREE.AmbientLight(0x404040, 3);
-    scene.add(ambientLight);
-
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshNormalMaterial();
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    // Animação
-    const animate = () => {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-      gl.endFrameEXP();
-    };
-    animate();
-  };
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.005;
+      meshRef.current.rotation.y += 0.005;
+    }
+  });
 
   return (
-    <View style={{ position: 'absolute', width: '100%', height: '100%', zIndex: -1 }}>
-      <GLView
-        style={{ flex: 1 }}
-        onContextCreate={onContextCreate}
-      />
-    </View>
+    <mesh ref={meshRef} position={[0, 0, 0]}>
+      <torusGeometry args={[1.5, 0.4, 16, 100]} />
+      <meshStandardMaterial color="purple" emissive="black" metalness={0.8} roughness={0.2} />
+    </mesh>
+  );
+}
+
+function DataNodes() {
+  return (
+    <group>
+      {[...Array(20)].map((_, i) => (
+        <Float key={i} speed={1.5} rotationIntensity={1} floatIntensity={2}>
+          <mesh position={[Math.sin(i) * 3, Math.cos(i) * 3, (i - 10) * 0.5]}>
+            <boxGeometry args={[0.2, 0.2, 0.2]} />
+            <meshStandardMaterial
+              color="#00ffcc"
+              emissive="#005f5f"
+              metalness={0.6}
+              roughness={0.3}
+            />
+          </mesh>
+        </Float>
+      ))}
+    </group>
+  );
+}
+
+export default function ThreeBackground() {
+  return (
+    <Canvas style={{ position: 'absolute', zIndex: -1 }} camera={{ position: [0, 0, 10], fov: 60 }}>
+      <color attach="background" args={["#000011"]} />
+      <ambientLight intensity={0.3} />
+      <pointLight position={[10, 10, 10]} intensity={1} />
+      <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade />
+
+      <SpinningTorus />
+      <DataNodes />
+
+      <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={0.5} />
+    </Canvas>
   );
 }
