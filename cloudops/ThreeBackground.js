f11d2a1c60001ee5c040ec5b2de5248f7baeaab4
Spacedwog <owner@spacedwog.com.br>
2025-05-18 14:10:32 -0300

    Commited

--- cloudops/ThreeBackground.js
+++ cloudops/ThreeBackground.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Stars, Float, Html } from '@react-three/drei';
-import * as THREE from 'three';
+import { loadAsync } from 'expo-three';
 
 function SpinningTorus() {
   const meshRef = useRef();
